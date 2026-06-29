4d289cb02a299bc9eeaf221fbc4f5672d170b783
aricloverEXTRA <157071384+aricloverEXTRA@users.noreply.github.com>
2026-06-29 13:35:58 -0500

    Update uYouPlusPatches.h

--- Sources/uYouPlusPatches.h
+++ Sources/uYouPlusPatches.h
@@ -4,6 +4,7 @@
 #import <YouTubeHeader/YTColorPalette.h>
 #import <YouTubeHeader/YTCommonColorPalette.h>
 #import <YouTubeHeader/YTSingleVideoController.h>
+#import <YouTubeHeader/ELMPBShowActionSheetCommand.h>
 #import <YouTubeHeader/GOODialogView.h>
 #import <YouTubeHeader/GPBDescriptor.h>
 #import <YouTubeHeader/GPBUnknownField.h>
