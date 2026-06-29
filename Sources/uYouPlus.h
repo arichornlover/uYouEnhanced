e91c52c2d95f0c0f428927a985740481c6eefa72
aricloverEXTRA <157071384+aricloverEXTRA@users.noreply.github.com>
2026-06-29 00:45:49 -0500

    Update uYouPlus.h

--- Sources/uYouPlus.h
+++ Sources/uYouPlus.h
@@ -350,11 +350,6 @@ static NSString *const kGoogleSigninFix = @"googleSigninFix_enabled";
 @interface ELMContainerNode : NSObject
 @end
 
-@interface ELMContainerNode (uYouEnhanced)
-- (void)applyRedColorToSubscribeButton:(id)view;
-- (void)hideMatchingSubviews:(id)view;
-@end
-
 @interface YTWrapperSplitView : UIView
 @end
 
